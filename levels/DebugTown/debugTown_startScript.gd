674c3cb1cd427dfd8aeaf8d027e1df3b7caf532d
Jake Trower <jakeonaut@gmail.com>
2019-03-26 22:13:42 -0700

    added music players

--- levels/DebugTown/debugTown_startScript.gd
+++ levels/DebugTown/debugTown_startScript.gd
@@ -1,13 +1,14 @@
 extends "res://scripts/levelScript.gd"
 
 func _ready():
-    ._ready() #super 
-    
-    # TODO(jaketrower): Should make a utility script 
+    ._ready() #super
+
+    # TODO(jaketrower): Should make a utility script
     # musicMaster.gd that controls stopping all players
     # and starting only the selected one.
     get_node("/root/DungeonThemePlayer").stop()
-        
+    get_node("/root/DebugTownThemePlayer").play()
+
     var player = get_node("Player")
     player.getCamera().rotateTo(0, true)
     player.getCamera().rotateXTo(-45, true)
--- scripts/PlayerSprite.gd
+++ scripts/PlayerSprite.gd
@@ -5,26 +5,26 @@ var hframe = 0
 onready var parent = get_parent()
 
 func _ready():
-	set_process(true)
+    set_process(true)
 
 func _process(delta):
-	if parent.linear_velocity.x != 0 or parent.linear_velocity.z != 0:
-		frame_delay = 0.2
-	elif Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right") \
-	or Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down"):
-		frame_delay = 0.3
-	else:
-		frame_delay = 0.4
-	animate(delta)
+    if parent.linear_velocity.x != 0 or parent.linear_velocity.z != 0:
+        frame_delay = 0.2
+    elif Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right") \
+    or Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down"):
+        frame_delay = 0.3
+    else:
+        frame_delay = 0.4
+    animate(delta)
 
 func setOverallsCostume():
-	hframe = 4
-	vframe = 0
-	updateStartframe()
+    hframe = 4
+    vframe = 0
+    updateStartframe()
 
 func setFloorGlitch():
-	pass
+    pass
 
 func updateStartFrame():
-	start_frame = (self.hframes * vframe) + hframe
-	set_frame(start_frame)
+    start_frame = (self.hframes * vframe) + hframe
+    set_frame(start_frame)
